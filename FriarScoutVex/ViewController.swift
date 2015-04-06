b8cafb874164c4133cc9edcf1ba6236a1efd7184
TheMkrage <metoofarmer@yahoo.com>
2015-04-06 10:57:14 -0700

    Initial Commit

---
+++ FriarScoutVex/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriarScoutVex
+//
+//  Created by Matthew Krager on 4/6/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
