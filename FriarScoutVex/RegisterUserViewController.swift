e0e1fc77b5c83630c74950f6c17ff7805d4f4e6a
TheMkrage <metoofarmer@yahoo.com>
2015-04-08 09:30:52 -0700

    AKPickerViewImlementation

--- FriarScoutVex/RegisterUserViewController.swift
+++ FriarScoutVex/RegisterUserViewController.swift
@@ -8,18 +8,52 @@
 
 import UIKit
 
-class RegisterUserViewController: UIViewController {
+
+class RegisterUserViewController: UIViewController, AKPickerViewDataSource, AKPickerViewDelegate {
     @IBOutlet var scrollView: UIScrollView!
+    
+    @IBOutlet var pickerView: AKPickerView!
+    
+    let titles = ["Tokyo", "Kanagawa", "Osaka", "Aichi", "Saitama", "Chiba", "Hyogo", "Hokkaido", "Fukuoka", "Shizuoka"]
+    
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.automaticallyAdjustsScrollViewInsets = false;
-                // Do any additional setup after loading the view.
+        
+        self.pickerView.delegate = self
+        self.pickerView.dataSource = self
+        
+        self.pickerView.font = UIFont(name: "BebasNeue", size: 20)!
+        self.pickerView.highlightedFont = UIFont(name: "BebasNeue", size: 20)!
+        self.pickerView.interitemSpacing = 20.0
+        self.pickerView.pickerViewStyle = .Flat
+        self.pickerView.reloadData()
+
     }
 
     override func viewDidLayoutSubviews() {
         self.scrollView.contentSize.height = 1000;
         self.scrollView.contentSize.width = self.view.frame.size.width;
 
+    }
+    
+   
+    
+    // MARK: - AKPickerViewDataSource
+    
+    func numberOfItemsInPickerView(pickerView: AKPickerView) -> Int {
+        return self.titles.count
+    }
+    
+    /*
+    Image Support
+    -------------
+    Please comment '-pickerView:titleForItem:' entirely and
+    uncomment '-pickerView:imageForItem:' to see how it works.
+    */
+    func pickerView(pickerView: AKPickerView, titleForItem item: Int) -> NSString {
+        return self.titles[item]
     }
 }
--- FriarScoutVex/ViewController.swift
+++ FriarScoutVex/ViewController.swift
@@ -8,17 +8,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet var passwordTextField: UITextField!
+    @IBOutlet var usernameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         //set the title to BebasNeue
         self.navigationController?.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "BebasNeue", size: 34)!]
+        
+        self.usernameTextField.delegate = self;
+        self.passwordTextField.delegate = self;
+    }
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        self.view.endEditing(true);
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        self.view.endEditing(true)
+        return false
     }
 
 
