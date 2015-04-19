512baa3a48bcfc40ed1b6d01546594dd9b238e1f
TheMkrage <metoofarmer@yahoo.com>
2015-04-19 10:05:58 -0700

    search bar displays but doesn't work :(

--- FriarScoutVex/TeamListTableViewController.swift
+++ FriarScoutVex/TeamListTableViewController.swift
@@ -8,36 +8,56 @@
 
 import UIKit
 
-class TeamListTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+class TeamListTableViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate, UISearchResultsUpdating, UISearchBarDelegate {
     var teams: NSMutableArray!
     var sorted: NSArray!
+    var controller: UISearchController!
     override func viewWillAppear(animated: Bool) {
         self.teams = NSMutableArray()
         var unsortedTeams = NSMutableArray()
         // Connect and order by key to place in array
         let ref = Firebase(url: "https://vexscout.firebaseio.com/teams")
         // Add all the values to an array
-        ref.queryOrderedByValue().observeEventType(.ChildAdded, withBlock: { snapshot -> Void in
+        ref.queryLimitedToFirst(50).observeEventType(.ChildAdded, withBlock: { snapshot -> Void in
             self.teams.addObject(snapshot.key as NSString)
             self.tableView.reloadData()
+            println("adding \(snapshot.key)");
         })
     }
+    
+    override func viewDidLoad() {
+        // Init Search Bar on Table
+        controller = UISearchController(searchResultsController: nil)
+        controller.searchResultsUpdater = self
+        controller.hidesNavigationBarDuringPresentation = false
+        controller.dimsBackgroundDuringPresentation = false
+        controller.searchBar.searchBarStyle = .Minimal
+        controller.searchBar.sizeToFit()
+        self.tableView.tableHeaderView = controller.searchBar
+       
+        //self.definesPresentationContext = true;
+
+    }
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if !(teams != nil) {
+            return 0
+        }
         return teams.count
     }
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         // Sorts numerically
-        sorted = self.sortArray(teams)
+        //sorted = self.sortArray(teams)
         
         // Creates cell and sets title to team num
         var cell = UITableViewCell()
-        cell.textLabel?.text = sorted.objectAtIndex(indexPath.row) as? String
+        cell.textLabel?.text = self.teams.objectAtIndex(indexPath.row) as? String
         return cell
     }
     
@@ -52,12 +72,40 @@ class TeamListTableViewController: UITableViewController, UITableViewDataSource,
         self.showViewController(vc as UITabBarController, sender: vc)
     }
     
+    
+    func filterContentForSearchText(searchText: String) {
+        let ref = Firebase(url: "https://vexscout.firebaseio.com/teams")
+        // Add all the values to an array
+        self.teams = NSMutableArray();
+        var lim = UInt(searchText.toInt()!)
+        ref.queryLimitedToFirst(lim).observeEventType(.ChildAdded, withBlock: { snapshot -> Void in
+            self.teams.addObject(snapshot.key as NSString)
+            self.tableView.reloadData()
+            println("adding \(snapshot.key)");
+        })
+
+    }
+    
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        var searchText: String?
+        searchText = searchController.searchBar.text;
+        if (searchText != nil) {
+            self.filterContentForSearchText(searchText!);
+        }
+    }
+    
+    
+    
+    /*func searchBar(searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+        self.updateSearchResultsForSearchController(self.searchController);
+    }*/
     // Sorts array by numbers of each string
     func sortArray(array: NSMutableArray) -> NSArray {
         let sortedArray = array.sortedArrayUsingComparator {
             (str1, str2) -> NSComparisonResult in
             let one = str1 as! NSString
             let two = str2 as! NSString
+            println("\(one) is compared than \(two)")
             return one.stringByTrimmingCharactersInSet(NSCharacterSet.letterCharacterSet()).compare(two.stringByTrimmingCharactersInSet(NSCharacterSet.letterCharacterSet()))
         }
         println(sortedArray)
