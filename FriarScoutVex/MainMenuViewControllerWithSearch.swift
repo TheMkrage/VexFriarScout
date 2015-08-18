c3831c8f66dc06c86c2667f5eca9b8181e71e83f
TheMkrage <metoofarmer@yahoo.com>
2015-08-17 22:51:42 -0700

    tables in tables

--- FriarScoutVex/MainMenuViewControllerWithSearch.swift
+++ FriarScoutVex/MainMenuViewControllerWithSearch.swift
@@ -15,23 +15,27 @@ class MainMenuViewControllerWithSearch: UIViewController, UITableViewDelegate, U
     
     @IBOutlet var tableView: UITableView!
     override func viewDidLoad() {
+        var s:Skills = Skills()
+        s.rank = "1"
+        s.team = "9983B"
+        s.score = "32"
+        self.robotSkills.addObject(s)
+        self.programmingSkills.addObject(s)
         self.tableView.delegate = self
         self.tableView.dataSource = self
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if tableView.isEqual(self.tableView) {
+            println("SELECT")
+        }
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         if tableView.isEqual(self.tableView) {
-            // Creates cell and sets title to team num
-            var array: NSMutableArray = NSMutableArray()
-            var s:Skills = Skills()
-            s.rank = "rank"
-            s.team = "team"
-            s.score = "score"
-            array.addObject(s)
             var cell = tableView.dequeueReusableCellWithIdentifier("CardCell") as! MainMenuTableCell
+            cell.selectionStyle = UITableViewCellSelectionStyle.None
             cell.setUp()
             cell.tableView.delegate = self
             cell.tableView.dataSource = self
@@ -51,22 +55,24 @@ class MainMenuViewControllerWithSearch: UIViewController, UITableViewDelegate, U
         }else {
             if let title:String = (tableView.superview?.superview?.superview as! MainMenuTableCell).titleLabel.text {
                 switch title {
-                    case "My Team":
-                        return tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
-                    case "Robot Skills":
-                        var cell = tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
-                        cell.rankLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).rank
-                        cell.teamLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).team
-                        cell.scoreLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).score
-                        return cell
-                    case "Programming Skills":
-                        var cell = tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
-                        cell.rankLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).rank
-                        cell.teamLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).team
-                        cell.scoreLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).score
-                        return cell
-                    default:
-                        return tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
+                case "My Team":
+                    return tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
+                case "Robot Skills":
+                    println("rs: \(indexPath.row)")
+                    var cell = tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
+                    cell.rankLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).rank
+                    cell.teamLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).team
+                    cell.scoreLabel.text = (self.robotSkills.objectAtIndex(indexPath.row) as! Skills).score
+                    return cell
+                case "Programming Skills":
+                    println(indexPath.row)
+                    var cell = tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
+                    cell.rankLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).rank
+                    cell.teamLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).team
+                    cell.scoreLabel.text = (self.programmingSkills.objectAtIndex(indexPath.row) as! Skills).score
+                    return cell
+                default:
+                    return tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
                 }
             }
             return tableView.dequeueReusableCellWithIdentifier("skillsCell") as! SkillsCell
@@ -74,7 +80,30 @@ class MainMenuViewControllerWithSearch: UIViewController, UITableViewDelegate, U
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        if !tableView.isEqual(self.tableView) {
+            println("EQUALS")
+            println(tableView.bounds)
+            if let title:String = (tableView.superview?.superview?.superview as! MainMenuTableCell).titleLabel.text {
+                println(title)
+                switch title {
+                case "My Team":
+                    return 0
+                case "Favorites":
+                    return 0
+                case "Robot Skills":
+                    println("ROBOT LENGTH: \(self.programmingSkills.count)")
+                    return self.robotSkills.count
+                case "Programming Skills":
+                    println("PROGRAMMING LENGTH: \(self.programmingSkills.count)")
+                    return self.programmingSkills.count
+                default:
+                    return 0
+                }
+            }
+            return 0
+        }else {
+            return 5
+        }
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
