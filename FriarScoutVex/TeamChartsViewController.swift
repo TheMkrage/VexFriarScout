75a30d76376c65196fba98fe0f8030c5c82dab43
TheMkrage <metoofarmer@yahoo.com>
2015-08-29 13:44:08 -0700

    chartsviewcontroller added

---
+++ FriarScoutVex/TeamChartsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TeamChartsViewController.swift
+//  FriarScoutVex
+//
+//  Created by Matthew Krager on 8/29/15.
+//  Copyright (c) 2015 Matthew Krager. All rights reserved.
+//
+
+import UIKit
+
+class TeamChartsViewController: UIViewController {
+    override func viewDidLoad() {
+        
+    }
+}
--- FriarScoutVex/UISettingsViewController.swift
+++ FriarScoutVex/UISettingsViewController.swift
@@ -7,7 +7,39 @@
 //
 
 import UIKit
+import Parse
 
-class UISettingsViewController: UIViewController {
-
+class UISettingsViewController: UIViewController, UITextFieldDelegate {
+    struct defaultsKeys {
+        static let myTeam = "myTeam"
+    }
+    
+    @IBOutlet var myTeamTextField: UITextField!
+    override func viewDidLoad() {
+        self.myTeamTextField.delegate = self
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        var query = PFQuery(className: "Teams")
+        query.whereKey("num", equalTo: self.myTeamTextField.text.uppercaseString)
+        if let x = query.findObjects() as? [PFObject] {
+            let defaults = NSUserDefaults.standardUserDefaults()
+            defaults.setValue(self.myTeamTextField.text.uppercaseString, forKey: defaultsKeys.myTeam)
+            defaults.synchronize()
+        }else {
+            // Alert the user that what they typed doesn't exist
+            let alertController = UIAlertController(title: "Oh no! ", message:
+                "That team doesn't exist!", preferredStyle: UIAlertControllerStyle.Alert)
+            alertController.addAction(UIAlertAction(title: "Oops!", style: UIAlertActionStyle.Default,handler: nil))
+        }
+        return true
+    }
 }
